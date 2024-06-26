c1dbe0efc6e250caa9920b9854d68ab8c5fc8aa9
minal <minalr421@gmail.com>
2024-06-26 15:38:31 +0530

    first commit changes

---
+++ projectd/p1.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 2.4
+num2 = 7.3
+num3 = 4.0
+# Add two numbers
+sum = num1 + num2+num3
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2,num3, sum))
